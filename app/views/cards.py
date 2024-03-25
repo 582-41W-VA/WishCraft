21d580fdaa076f10bb7f7af2a0997524dd42f40d
Maiko <rnaico79@gmail.com>
2024-03-25 19:17:30 -0400

    Create modules and import them

--- app/views/cards.py
+++ app/views/cards.py
@@ -1,70 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.models import User, auth
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
-from .models import Card, Tag, Comment
-
-# Create your views here.
-
-def create_user_if_not_exists(username, email, password, request):
-    if User.objects.filter(username=username).exists():
-        messages.error(request, "Username already exists")
-        return False
-    else:
-        user = User.objects.create_user(
-            username=username, email=email, password=password
-        )
-        user.save()
-        print("User created")
-        return True
+from ..models import Card, Tag, Comment
 
 
-def register(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        email = request.POST["email"]
-        password = request.POST["password"]
-        confirm_password = request.POST["confirm_password"]
-
-        # Check if passwords match
-        if password != confirm_password:
-            messages.error(request, "Passwords do not match")
-            return render(request, "app/register.html")
-
-        # Attempt to create the user
-        if create_user_if_not_exists(username, email, password, request):
-            return redirect("login")
-        else:
-            return redirect(register)
-    else:
-        return render(request, "app/register.html")
-
-
-def login(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = auth.authenticate(username=username, password=password)
-        if user is not None:
-            auth.login(request, user)
-            print("User logged in")
-            if request.user.is_superuser:
-                return render(request, "app/admin.html")
-            return redirect("user_list")
-        else:
-            messages.error(request, "Invalid credentials")
-            return redirect("login")
-    else:
-        return render(request, "app/login.html")
-
-
-def logout(request):
-    auth.logout(request)
-    return redirect("home")
-
-
-# Retrieve all images from the database
-# filter, search, and sort
 def home(request):
     cards = Card.objects.all()
     tags = Tag.objects.all()
--- wishcraft/urls.py
+++ wishcraft/urls.py
@@ -19,10 +19,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from app import views
+from app.views.cards import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("app/", include("app.urls")),
-    path("", views.home, name="home"),
+    path("", home, name="home"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
