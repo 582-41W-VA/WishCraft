ee6acafb208973ba0d76900a82f2cc8312a5df9b
Maiko <rnaico79@gmail.com>
2024-03-25 17:50:45 -0400

    Add path

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
+    path("card/<int:card_id>/", views.card_detail, name="card_detail"),
+    path("card/<int:card_id>/add_comment/", views.add_comment, name="add_comment"),
+    path("<int:card_id>/increment_likes/", views.increment_likes, name="increment_likes"),
+    path("create_card/", views.create_card, name="create_card"),
+    path("your_wish_list/", views.user_list, name="user_list")
 ]
