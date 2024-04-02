35c719a64acb13d82028639a0ee4bfd4a1b356d0
Maiko <rnaico79@gmail.com>
2024-04-02 01:19:52 -0400

    Fix issue #10, close #10

--- app/models.py
+++ app/models.py
@@ -24,8 +24,21 @@ class Card(models.Model):
         return self.title
 
 
+DEFAULT_TAGS = [
+        ("fashion", "Fashion"),
+        ("home decor", "Home Decor"),
+        ("food & recipe", "Food & Recipe"),
+        ("travel", "Travel"),
+        ("art & design", "Art & Design"),
+        ("photography", "Photography"),
+        ("diy & craft", "DIY & Craft"),
+        ("health & fitness", "Health & Fitness"),
+        ("tech", "Tech"),
+        ("books & literature", "Books & Literature"),
+    ]
+
 class Tag(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, choices=DEFAULT_TAGS)
 
     def __str__(self):
         return self.name
