5b82782726ec9a1dd66abc884c2ad7e5a224aba1
Maiko <rnaico79@gmail.com>
2024-04-09 15:52:12 -0400

    Relocate inline script tag to script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,23 +1,54 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Home Page
+    // the sort form to submit the form when the value changes
     const sortForm= document.getElementById("sortForm");
-    sortForm.addEventListener("change", function() {
-        this.submit();
-    });
+    if (sortForm) {
+        sortForm.addEventListener("change", function() {
+            this.submit();
+        });
+    }
 
+    // the filter form to submit the form when the value changes
     const filterForm = document.getElementById("filterForm");
     const tagCheckboxes = document.querySelectorAll("input[name='tags']");
-    tagCheckboxes.forEach(function(checkbox) {
-        checkbox.addEventListener("change", function() {
+    if (filterForm) {
+        tagCheckboxes.forEach(function(checkbox) {
+            checkbox.addEventListener("change", function() {
+                filterForm.submit();
+            });
+        });
+    }
+
+    // the clear tags button to uncheck all checkboxes
+    const clearTagsBtn = document.getElementById("clearTagsBtn");
+    if (clearTagsBtn && filterForm) {
+        clearTagsBtn.addEventListener("click", function() {
+            let checkboxes = document.querySelectorAll("input[type='checkbox'][name='tags']");
+            checkboxes.forEach(function(checkbox) {
+                checkbox.checked = false;
+            });
             filterForm.submit();
         });
-    });
+    }
 
-    const clearTagsBtn = document.getElementById("clearTagsBtn");
-    clearTagsBtn.addEventListener("click", function() {
-        let checkboxes = document.querySelectorAll("input[type='checkbox'][name='tags']");
-        checkboxes.forEach(function(checkbox) {
-            checkbox.checked = false;
+    // Admin Panel
+    // the delete user form to confirm the deletion
+    const deleteUser = document.getElementById('deleteUser');
+    if (deleteUser) {
+        deleteUser.addEventListener('submit', function(event) {
+            if (!confirm('Are you sure you want to delete this user? This action cannot be undone.')) {
+                event.preventDefault();
+            }
         });
-        filterForm.submit();
+    }
+
+    // the delete card form to confirm the deletion
+    let deleteForm = document.querySelectorAll('.deleteCard');
+    deleteForm.forEach(function(form) {
+        form.addEventListener('submit', function(event) {
+            if (!confirm('Are you sure you want to delete this card? This action cannot be undone.')) {
+                    event.preventDefault();
+            }
+        });
     });
 });
